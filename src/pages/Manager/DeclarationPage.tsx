dd9a41c096a41e2ef55cb779607914b81c28dc18
161-Keshav <ppg.gnanakeshav@gmail.com>
2025-09-07 17:28:51 +0530

    Added Dummy Pages for Manager Routes

---
+++ src/pages/Manager/DeclarationPage.tsx
@@ -0,0 +1,8 @@
+const DeclarationsPage =() =>
+{
+    return <h1>
+        This is Declarations Page.
+    </h1>
+}
+
+export default DeclarationsPage;
---
+++ src/pages/Manager/DetailsPage.tsx
@@ -0,0 +1,10 @@
+const DetailsPage =() =>
+{
+    return (
+        <h1>
+            This is Details Page.
+        </h1>
+    )
+}
+
+export default DetailsPage;
